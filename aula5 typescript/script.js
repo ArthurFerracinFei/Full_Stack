8f9ed897624f059e886c3175527d3959f69946ad
Arthur Ferracin <arthur.ferracin02@gmail.com>
2025-09-11 17:12:21 -0300

    Aula 5

---
+++ aula5 typescript/script.js
@@ -0,0 +1,33 @@
+//window.alert("olá mundo")
+//console.log("olá mundo")
+
+nome = prompt("qual seu nome?");
+console.log("Olá " + nome);
+
+a = 1;
+b = 2;
+if(a < b){
+    //
+}
+else if(b < a){
+
+}
+else{
+
+}
+
+i = 0;
+
+//while(i <= 100){
+ //   console.log(i);
+//    i++;
+//}
+
+console.log("Exercicio 1 - Desenvolver um algoritmo em Javascript que exiba os números ínpares de 1 a 100")
+
+i = 1
+while(i < 100){
+    console.log(i);
+    i += 2;
+    
+}
